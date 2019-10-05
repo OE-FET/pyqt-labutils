d737ee2c315963c75802b1aafe7eedcd6da2004d
Sam Schott <ss2151@cam.ac.uk>
2019-10-05 23:40:42 +0100

    added module for dark mode detection

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from .list_entry_widget import FloatListWidget
 from .notify import Notipy
 from .scientific_spinbox import ScienSpinBox, ScienDSpinBox
 from .settings_pane import SettingsWidget
+from .dark_mode_support import isDarkWindow, LINE_COLOR_DARK, THEME_LIGHT
---
+++ dark_mode_support.py
@@ -0,0 +1,35 @@
+from qtpy import QtWidgets, QtGui, Qt
+
+
+THEME_DARK = "dark"
+THEME_LIGHT = "light"
+
+LINE_COLOR_DARK = (70, 70, 70)
+LINE_COLOR_LIGHT = (213, 213, 213)
+
+
+def rgb_to_luminance(r, g, b, base=256):
+    """
+    Calculates luminance of a color, on a scale from 0 to 1, meaning that 1 is the
+    highest luminance. r, g, b arguments values should be in 0..256 limits, or base
+    argument should define the upper limit otherwise.
+    """
+    return (0.2126*r + 0.7152*g + 0.0722*b)/base
+
+
+def windowTheme():
+    """
+    Returns one of gui.utils.THEME_LIGHT or gui.utils.THEME_DARK, corresponding to
+    current user's UI theme.
+    """
+    # getting color of a pixel on a top bar, and identifying best-fitting color
+    # theme based on its luminance
+    w = QtWidgets.QWidget()
+    bg_color = w.palette().color(QtGui.QPalette.Background)
+    bg_color_rgb = [bg_color.red(), bg_color.green(), bg_color.blue()]
+    luminance = rgb_to_luminance(*bg_color_rgb)
+    return THEME_LIGHT if luminance >= 0.4 else THEME_DARK
+
+
+def isDarkWindow():
+    return windowTheme() == THEME_DARK
