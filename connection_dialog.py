52865e89ef83dae9b730a3f5d446f3dc0d7b1f26
Sam Schott <ss2151@cam.ac.uk>
2019-12-13 16:50:59 +0000

    fixup

--- connection_dialog.py
+++ connection_dialog.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtWidgets, uic
 
 from .animated_widgets import AnimatedResizeWidget
 
-basedir = osp.basename(osp.abspath(__file__))
+basedir = osp.dirname(osp.abspath(__file__))
 CONNECTION_UI_PATH = osp.join(basedir, 'connection_dialog.ui')
 
 
